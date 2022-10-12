8b2d4608e2237db2bb0f2abb901fb0ff02216283
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2022-10-12 08:25:12 -0400

    Add files via upload

---
+++ EXAMENPRO1SIVECTORES/EXAMENPRO1SIVECTORES/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace EXAMENPRO1SIVECTORES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mENUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARGARRNDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dESCARGARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXAMENToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pREGUNTA2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Numero de elementos";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(124, 326);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(529, 20);
+            this.textBox8.TabIndex = 27;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(124, 273);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(529, 20);
+            this.textBox7.TabIndex = 26;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(124, 225);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(529, 20);
+            this.textBox6.TabIndex = 25;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(124, 172);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(529, 20);
+            this.textBox5.TabIndex = 24;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(409, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 22;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(269, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mENUToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(774, 24);
+            this.menuStrip1.TabIndex = 29;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mENUToolStripMenuItem
+            // 
+            this.mENUToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cARGARRNDToolStripMenuItem,
+            this.dESCARGARToolStripMenuItem,
+            this.eXAMENToolStripMenuItem,
+            this.pREGUNTA2ToolStripMenuItem});
+            this.mENUToolStripMenuItem.Name = "mENUToolStripMenuItem";
+            this.mENUToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.mENUToolStripMenuItem.Text = "MENU";
+            // 
+            // cARGARRNDToolStripMenuItem
+            // 
+            this.cARGARRNDToolStripMenuItem.Name = "cARGARRNDToolStripMenuItem";
+            this.cARGARRNDToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cARGARRNDToolStripMenuItem.Text = "CARGAR RND";
+            this.cARGARRNDToolStripMenuItem.Click += new System.EventHandler(this.cARGARRNDToolStripMenuItem_Click);
+            // 
+            // dESCARGARToolStripMenuItem
+            // 
+            this.dESCARGARToolStripMenuItem.Name = "dESCARGARToolStripMenuItem";
+            this.dESCARGARToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dESCARGARToolStripMenuItem.Text = "DESCARGAR";
+            this.dESCARGARToolStripMenuItem.Click += new System.EventHandler(this.dESCARGARToolStripMenuItem_Click);
+            // 
+            // eXAMENToolStripMenuItem
+            // 
+            this.eXAMENToolStripMenuItem.Name = "eXAMENToolStripMenuItem";
+            this.eXAMENToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eXAMENToolStripMenuItem.Text = "PREGUNTA 1";
+            // 
+            // pREGUNTA2ToolStripMenuItem
+            // 
+            this.pREGUNTA2ToolStripMenuItem.Name = "pREGUNTA2ToolStripMenuItem";
+            this.pREGUNTA2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pREGUNTA2ToolStripMenuItem.Text = "PREGUNTA 2";
+            this.pREGUNTA2ToolStripMenuItem.Click += new System.EventHandler(this.pREGUNTA2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 428);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mENUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXAMENToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARGARRNDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dESCARGARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pREGUNTA2ToolStripMenuItem;
+    }
+}
+
---
+++ EXAMENPRO1SIVECTORES/EXAMENPRO1SIVECTORES/vector.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMENPRO1SIVECTORES
+{
+    class vector
+    {
+        const int MAX = 100;
+        private int[] v;
+        private int n;
+        public vector()
+        {
+            n = 0;
+            v = new int[MAX];
+        }
+
+
+        //public void cargardato(int nele)
+        //{
+        //    n = nele;//JCE
+        //    int num = n;
+        //    for (int i = 1; i <= num; i = checked(i + 1))
+        //    {//JCE
+        //        v[i] = Conversions.ToInteger(Interaction.InputBox("ingrese elemento", "elemento"));
+        //    }
+        //}
+
+
+
+        public void Cargar_rnd(int n1, int a, int b)
+        {
+            Random r;
+            r = new Random();
+            int i;
+            n = n1;
+            for (i = 1; i <= n; i++)
+            {
+                v[i] = r.Next(a, b);
+            }
+
+        }
+
+
+        public string descargar()
+        {
+            string s;
+            int i;
+            s = "";
+            for (i = 1; i <= n; i++)
+            {
+                s = s + v[i] + "  |  ";
+            }
+            return s;
+        }
+        //
+        public int Elem_Masfrec_seg(int a, int b)
+        {
+            int i = a;
+            int fr = Frecuencia(v[i]);
+            int ele = v[i];
+            i = a;
+            while (true)
+            {
+                int num = i;
+                int num2 = b;
+                //if (num > num2)
+                    if (num > num2)
+                    {
+                    break;
+                }
+                //if (Frecuencia(v[i]) > fr)
+                    if (Frecuencia(v[i]) < fr)
+                    {
+                    fr = Frecuencia(v[i]);
+                    ele = v[i];
+                }
+                i = checked(i + 1);
+            }
+            return ele;
+        }
+
+        public int pregunta2(int a, int b)
+        {
+            int i, fr, ele;
+            i = a;
+            fr = Frecuencia(v[i]);
+            ele = v[i];
+
+            for (i=a;i<=b;i++)
+            {
+                if (Frecuencia(v[i])>fr)
+                {
+                    fr = Frecuencia(v[i]);
+                    ele = v[i];
+                }
+            }
+            return ele;
+        }
+
+        //
+
+
+        public int Frecuencia(int ele)
+        {
+            int num = n;
+            int i = 1;
+            checked
+            {
+                int fr = default(int);
+                while (true)
+                {
+                    int num2 = i;
+                    int num3 = num;
+                    if (num2 > num3)
+                    {
+                        break;
+                    }
+                    if (ele == v[i])
+                    {
+                        fr++;
+                    }
+                    i++;
+                }
+                return fr;
+            }
+        }
+
+        public int frecuenciaAB(int ele, int a, int b)
+        {
+            int c = 0;
+            int i = a;
+            checked
+            {
+                while (true)
+                {
+                    int num = i;
+                    int num2 = b;
+                    if (num > num2)
+                    {
+                        break;
+                    }
+                    if (v[i] == ele)
+                    {
+                        c++;
+                    }
+                    i++;
+                }
+                return c;
+            }
+        }
+
+
+
+
+
+
+    }
+}
